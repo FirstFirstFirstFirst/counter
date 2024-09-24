c47b8a1c8b7207078d61ecfcda20a12bb325c549
FirstFirstFirstFirst <hocrux1st.first@gmail.com>
2024-09-24 14:11:40 +0700

    down grade build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 TAG=v1
 DOCKER_HUB=first022/counter:$TAG
 
-cp .env .env.docker
-
 docker login
-docker build --env-file .env.docker -t $DOCKER_HUB .
+docker build -t $DOCKER_HUB .
 docker push $DOCKER_HUB
-
-rm .env.docker
